0a1154e831ad899c6eb76de306d99f40ed3a8311
Steven01231 <100371532+Steven01231@users.noreply.github.com>
2026-03-26 18:15:48 -0400

    added EditEventActivity

---
+++ app/src/main/java/com/example/soen345_winter2026/admin/EditEventActivity.kt
@@ -0,0 +1,9 @@
+package com.example.soen345_winter2026.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.soen345_winter2026.databinding.AddEventBinding
+
+class EditEventActivity: AppCompatActivity() {
+
+    private lateinit var binding: AddEventBinding
+}
